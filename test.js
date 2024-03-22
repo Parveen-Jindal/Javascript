19dc0ba8ffa4d920232f3d0207a6eeab46ca2eb0
parveen-jindal <parveenjindal5384@gmail.com>
2024-03-22 22:46:25 +0530

    variables

--- test.js
+++ test.js
@@ -1,1 +1,8 @@
-console.log("Parveen")
+const accountId = "12345"
+let accountEmail ="parveenjindal5384@gmail.com"
+let accountCity = null
+
+// console.log("Parveen")
+// console.table([accountId, accountEmail, accountCity])  
+
+console.log(accountId > 1);   
